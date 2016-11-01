fd03c31644cba16580640324afd1380ee33f9993
pankaj_dighe <pankajdighe77@gmail.com>
2016-10-31 23:37:26 -0700

    Game working

--- game_logic.js
+++ game_logic.js
@@ -1,7 +1,38 @@
 var myMap = new Map();
 
 			 $( document ).ready(function() {
-       			 console.log( "document loaded" );
+
+			 	console.log( "document loaded" );
+       			
+       			setGame();
+    		});
+
+			
+
+
+		    function tableText(tableCell) {
+		    	tableCell.style.backgroundColor = "yellow"
+
+				var colIndex = $("#mindsweeper_table td").index(tableCell);
+
+				if(myMap.get(colIndex)==undefined){
+					console.log("");
+
+					findNeighbours(tableCell,colIndex);
+
+				}else{
+
+					tableCell.style.backgroundColor = "red"
+
+					console.log("Clicked On Bomb");
+					resetGame();
+				}
+		      //	alert(colIndex);
+		    }
+
+
+		    function setGame(){
+
 
        			 var table = document.getElementById("mindsweeper_table");
 
@@ -29,25 +60,57 @@ var myMap = new Map();
 
 		        				
 		    }
-    		});
 
-			
 
-		    function tableText(tableCell) {
-		    	tableCell.style.backgroundColor = "yellow"
 
-				var colIndex = $("#mindsweeper_table td").index(tableCell);
+		    }
 
-				if(myMap.get(colIndex)==undefined){
-					console.log("");
 
-				}else{
+		    function resetGame(){
 
-					tableCell.style.backgroundColor = "red"
+		    	window.location.reload();
+		    }
 
-					console.log("Clicked On Bomb");
-				}
-		      //	alert(colIndex);
+		    function findNeighbours(tableCell,cell){
+
+		    	var row_v=[-1,1,-1,-1,0,1,1,0];
+		    	var col_v=[-1,1,0,1,1,0,-1,-1];
+
+		    	console.log("row is "+parseInt(cell/8));
+		    	console.log("col is "+parseInt(cell%8));
+
+		    	var row=parseInt(cell/8);
+		    	var col=parseInt(cell%8);
+
+		    	var max_row=7;
+		    	var max_col=7;
+		    	var count=0;
+
+		    	for(var i=0;i<8;i++){
+
+		    		var temp_row=row+row_v[i];
+		    		var temp_col=col+col_v[i];
+		    		var elementNo=temp_row*8+temp_col;
+
+		    		if(temp_row>=0 && temp_col>=0 && temp_row<=max_row && temp_col<=max_col){
+		    		console.log("Pair ("+(row+row_v[i])+","+(col+col_v[i])+")");
+		    		console.log("Reverting "+(temp_row*8+temp_col));
+
+		    		if(myMap.get(elementNo)!=undefined){
+
+		    			count++;
+		    		}
+
+
+
+		    	}
+
+		    	}
+
+		    	tableCell.innerHTML = count;
+
+
+
 		    }
 
 
